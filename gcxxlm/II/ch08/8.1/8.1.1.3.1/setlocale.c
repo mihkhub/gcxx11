84cd39af57e2b849197131ad22b9162d9a1b57d3
Issac Chun <changfeng.or.chen79@gmail.com>
2018-02-03 04:18:18 +0800

    use setlocale to temporarily switch to a new locale
    Fix #11

---
+++ gcxxlm/II/ch08/8.1/8.1.1.3.1/setlocale.c
@@ -0,0 +1,50 @@
+#include <stddef.h>
+
+#include <locale.h>
+
+#include <stdlib.h>
+
+#include <string.h>
+#include <stdio.h>
+
+void
+
+with_other_locale (char *new_locale,
+
+		void (*subroutine) (int),
+
+		int argument)
+
+{
+
+	char *old_locale, *saved_locale;
+
+	/* Get the name of the current locale. */
+
+	old_locale = setlocale (LC_ALL, NULL);
+
+	/* Copy the name so it won’t be clobbered by setlocale. */
+
+	saved_locale = strdup (old_locale);
+
+	if (saved_locale == NULL)
+
+		perror("Out of memory");
+
+	/* Now change the locale and do some stuff with it. */
+
+	setlocale (LC_ALL, new_locale);
+
+	(*subroutine) (argument);
+	/* Restore the original locale. */
+
+	setlocale (LC_ALL, saved_locale);
+
+	free (saved_locale);
+
+}
+
+int main(int argc, char **argv)
+{
+	return 0;
+}
